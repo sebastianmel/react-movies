2ba558611b0e4f82b3f393ae903d897c6bd8ac00
sebastianmel <57873668+sebastianmel@users.noreply.github.com>
2021-11-15 22:39:13 +0100

    step 3

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const footer = () => {
+    return (
+        <footer className="footer">
+            <p> ©React Movie by sébastian</p>
+        </footer>
+    );
+};
+
+export default footer;
